9042388aadc144e4d928f8820c63045e9b8bbadb
goen <fuuucl.am@gmail.com>
2014-10-13 18:30:31 +0200

    The tracker will be used by the other tools to track the binaries called

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -6,6 +6,14 @@ import (
 
 func main() {
 	fmt.Println("HELLO FROM TRACKER")
-
+	// TODO: dump ENV
+	fmt.Println("TODO:dump ENV")
+	// TODO: dump ARGS
+	fmt.Println("TODO:dump ARGS")
+	// TODO: dump ARGS
+	fmt.Println("TODO:run the actual binary")
+	// TODO: wait
+	fmt.Println("TODO:wait for the actual binary to complete")
+	/**/
 	fmt.Println("BYE BYE FROM TRACKER")
 }
