41bb64be98b173bbbaff41fad88223e4dd8c35df
goen <fuuucl.am@gmail.com>
2014-10-16 15:15:26 +0200

    Remove the scaffolding reference filesystem implementation
    It is available in the bazil repo as hellofs

--- loopfs.go
+++ loopfs.go
@@ -4,7 +4,7 @@ import (
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 
-	"fmt"
+	_ "fmt"
 	"io/ioutil"
 	"os"
 	"time"
@@ -90,70 +90,4 @@ func (looperfile) Attr() fuse.Attr {
 	a.Mode = 0555
 	a.Nlink = 1 // correct?//FIXME
 	return a
-}
-
-// LoopFS implements the loop part of the fs.
-// A FUSE filesystem that shunts all request to an underlying file
-// system.
-type LoopFS struct{}
-
-func (LoopFS) Root() (fs.Node, fuse.Error) {
-	if debug {
-		fmt.Println("LOOPFS::ROOT")
-	}
-	return Dir{}, nil
-}
-
-// Dir implements both Node and Handle for the root directory.
-type Dir struct{}
-
-func (Dir) Attr() fuse.Attr {
-	if debug {
-		fmt.Println("DIR::ATTR")
-	}
-
-	return fuse.Attr{Inode: 1, Mode: os.ModeDir | 0555}
-}
-func (Dir) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
-	if debug {
-		fmt.Println("DIR::LOOKUP:", name)
-	}
-
-	if name == "hello" {
-		return File{}, nil
-	}
-	return nil, fuse.ENOENT
-}
-
-var dirDirs = []fuse.Dirent{
-	{Inode: 2, Name: "hello", Type: fuse.DT_File},
-	{Inode: 3, Name: "world", Type: fuse.DT_File},
-}
-
-func (Dir) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
-	if debug {
-		//		fmt.Println("DIR::ReadDir")
-	}
-
-	return dirDirs, nil
-}
-
-// File implements both Node and Handle for the hello file.
-type File struct{}
-
-const greeting = "hello, world\n"
-
-func (File) Attr() fuse.Attr {
-	if debug {
-		fmt.Println("FILE::attr")
-	}
-
-	return fuse.Attr{Inode: 2, Mode: 0444, Size: uint64(len(greeting))}
-}
-func (File) ReadAll(intr fs.Intr) ([]byte, fuse.Error) {
-	if debug {
-		fmt.Println("FILE::readall")
-	}
-
-	return []byte(greeting), nil
 }
