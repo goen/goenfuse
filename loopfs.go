917b7371f981b79be55ee67f134881caf5b27ece
goen <fuuucl.am@gmail.com>
2014-10-16 15:10:40 +0200

    Start implementing the loop fs
    loopfs will be used for tracking file reads and writes

--- loopfs.go
+++ loopfs.go
@@ -6,7 +6,45 @@ import (
 
 	"fmt"
 	"os"
+	"time"
 )
+
+//ok
+func lgeneric_attr() fuse.Attr {
+	u := time.Now()
+	return fuse.Attr{
+		Atime: u, Mtime: u, Ctime: u, Crtime: u,
+		Uid:   uint32(os.Geteuid()),
+		Gid:   uint32(os.Getegid()),
+		Rdev:  0x70F5, //LOop-FS
+		Flags: 0,
+	}
+}
+
+//ok
+type looperfs struct {
+}
+
+type looperdir struct {
+}
+
+type looperfile struct {
+}
+
+// get fs root node
+func (looperfs) Root() (fs.Node, fuse.Error) {
+	return looperdir{}, nil
+}
+
+func (looperdir) Attr() fuse.Attr {
+	a := lgeneric_attr()
+	a.Inode = 1
+	a.Size = 4096
+	a.Blocks = 8
+	a.Mode = os.ModeDir | 0555
+	a.Nlink = 8 // correct?//FIXME
+	return a
+}
 
 // LoopFS implements the loop part of the fs.
 // A FUSE filesystem that shunts all request to an underlying file
