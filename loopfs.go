a948c1d0dffd20231cd73d211654d87c629c3fb4
goen <fuuucl.am@gmail.com>
2014-10-18 19:37:11 +0200

    We implement a sample new Attr that will enable the loopback
    The loopback will have in stat all the fields from the original files

--- loopfs.go
+++ loopfs.go
@@ -4,10 +4,14 @@ import (
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 
-	_ "fmt"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"time"
+)
+
+const (
+	loopfs_rdev = 0x70F5 //LOop-FS
 )
 
 //ok
@@ -20,6 +24,54 @@ func lgeneric_attr() fuse.Attr {
 		Rdev:  0x70F5, //LOop-FS
 		Flags: 0,
 	}
+}
+
+func (looperdir) Attr() fuse.Attr {
+	a := lgeneric_attr()
+	a.Inode = 1
+	a.Size = 4096
+	a.Blocks = 8
+	a.Mode = os.ModeDir | 0555
+	a.Nlink = 8 // correct?//FIXME
+	return a
+}
+
+func (looperfile) Attr() fuse.Attr {
+	a := lgeneric_attr()
+	a.Inode = 2
+	a.Size = 4096
+	a.Blocks = 8
+	a.Mode = 0555
+	a.Nlink = 1 // correct?//FIXME
+	return a
+}
+
+func to(s Stat) fuse.Attr {
+	return fuse.Attr{
+		Inode:  s.Inode(),
+		Size:   s.Size_(),
+		Blocks: s.Blocks_(),
+		Atime:  s.Atime(),
+		Mtime:  s.Mtime(),
+		Ctime:  s.Ctime(),
+		Crtime: s.Crtime(),
+		Mode:   s.Mode_(),
+		Nlink:  s.Nlink_(),
+		Uid:    s.Uid,
+		Gid:    s.Gid,
+		Rdev:   s.Rdev_(),
+		Flags:  s.Flags(),
+	}
+}
+
+func chinode(f fuse.Attr) fuse.Attr {
+	f.Inode ^= 0x7FFF
+	return f
+}
+
+func putrdev(f fuse.Attr) fuse.Attr {
+	f.Rdev = loopfs_rdev
+	return f
 }
 
 //ok
@@ -35,29 +87,24 @@ type looperfile struct {
 }
 
 // get fs root node
-func (l looperfs) Root() (fs.Node, fuse.Error) {
+func (looperfs) Root() (fs.Node, fuse.Error) {
 	return looperdir{name: "."}, nil
 }
 
-func (looperdir) Attr() fuse.Attr {
-	a := lgeneric_attr()
-	a.Inode = 1
-	a.Size = 4096
-	a.Blocks = 8
-	a.Mode = os.ModeDir | 0555
-	a.Nlink = 8 // correct?//FIXME
-	return a
+/*
+func (l looperdir) Attr() fuse.Attr {
+	return looperfile{name: l.name}.Attr()
 }
-
+*/
 func (l looperdir) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
 	if l.name == "." && (name == mpoint_gloop || name == mpoint_gbin) {
 		return nil, fuse.ENOENT
 	}
 
 	fi, err := os.Lstat(l.name + "/" + name)
-
 	if err != nil {
-		return nil, fuse.ENOENT
+		return looperdir{name: l.name}, nil
+		//		return nil, fuse.ENOENT
 	}
 
 	if fi.IsDir() {
@@ -66,10 +113,13 @@ func (l looperdir) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
 		return looperfile{name: l.name + "/" + name}, nil
 	}
 
+	//	return looperfile{name: l.name + "/" + name}, nil
 	return nil, fuse.ENOENT
 }
 
 func (l looperdir) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
+
+	fmt.Println("reading dir:", l.name)
 
 	fi, err := ioutil.ReadDir(l.name)
 	if err != nil {
@@ -92,12 +142,13 @@ func (l looperdir) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
 	return dirz, nil
 }
 
-func (looperfile) Attr() fuse.Attr {
-	a := lgeneric_attr()
-	a.Inode = 2
-	a.Size = 4096
-	a.Blocks = 8
-	a.Mode = 0555
-	a.Nlink = 1 // correct?//FIXME
-	return a
+/*
+func (l looperfile) Attr() fuse.Attr {
+	a, err := os.Lstat(l.name)
+	if err != nil {
+		return fuse.Attr{}
+	}
+
+	return putrdev(chinode(to(StatSys(a))))
 }
+*/
