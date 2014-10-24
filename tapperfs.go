dd442cb8b2f2d46ad44aa84af8e8045005f0e507
goen <fuuucl.am@gmail.com>
2014-10-24 13:49:20 +0200

    Visible directories in tapperfs

--- tapperfs.go
+++ tapperfs.go
@@ -9,6 +9,7 @@ import (
 
 	"fmt"
 	"os"
+	"time"
 )
 
 func tapcontext(i [][]string, z *self) nodefs.Node {
@@ -19,6 +20,26 @@ type tapper_root struct {
 	nodefs.Node
 	itemz [][]string // len(itemz) = 1 + maximum name
 	*self
+}
+
+type tappertrackernode struct {
+	nodefs.Node
+	*self
+	f *os.File
+}
+
+type tapperdirnode struct {
+	nodefs.Node
+	i     uint64 //name = i, inode = i + inodeoffset
+	itemz []string
+}
+
+func nrn(s *self) *tappertrackernode {
+	return &tappertrackernode{Node: nodefs.NewDefaultNode(), self: s}
+}
+
+func ndn(i int, itemz []string) *tapperdirnode {
+	return &tapperdirnode{Node: nodefs.NewDefaultNode(), i: uint64(i), itemz: itemz}
 }
 
 func (r tapper_root) OpenDir(context *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
@@ -42,4 +63,142 @@ func (r tapper_root) OpenDir(context *fuse.Context) ([]fuse.DirEntry, fuse.Statu
 	sdirs := dirz[0 : end+foffset]
 
 	return sdirs, fuse.OK
+}
+
+func (r tapper_root) Lookup(out *fuse.Attr, name string, context *fuse.Context) (node *nodefs.Inode, code fuse.Status) {
+	if name == "tracker" || name == ".track" || name == ".untrack" {
+		out.Mode = fuse.S_IFREG | 0644
+		ch := r.Inode().NewChild(name, false, nrn(r.self))
+		return ch, fuse.OK
+	}
+
+	var i int
+
+	n, err := fmt.Sscanf(name, "%02d", &i)
+
+	if (err != nil) || (n != 1) || (i >= len(r.itemz)) {
+		return nil, fuse.ENOENT
+	}
+
+	out.Mode = fuse.S_IFDIR | 0755
+	out.Size = 4096
+	ch := r.Inode().NewChild(name, false, ndn(i, r.itemz[i]))
+
+	return ch, fuse.OK
+}
+func (tapperdirnode) GetAttr(out *fuse.Attr, file nodefs.File, context *fuse.Context) (code fuse.Status) {
+
+	out.Mode = fuse.S_IFDIR | 0755
+
+	return fuse.OK
+}
+func (tappertrackernode) GetAttr(out *fuse.Attr, file nodefs.File, context *fuse.Context) (code fuse.Status) {
+
+	out.Mode = fuse.S_IFREG | 0644
+
+	return fuse.OK
+}
+
+func (tapper_root) OnUnmount() {
+	fmt.Println("001")
+}
+func (tapper_root) OnMount(conn *nodefs.FileSystemConnector) {
+	fmt.Println("002")
+}
+func (tapper_root) StatFs() *fuse.StatfsOut {
+	fmt.Println("003")
+	return nil
+}
+
+func (tapper_root) Deletable() bool {
+	fmt.Println("004")
+	return true
+}
+
+func (tapper_root) OnForget() {
+	fmt.Println("005")
+}
+
+func (tapper_root) Access(mode uint32, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Readlink(c *fuse.Context) ([]byte, fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Mknod(name string, mode uint32, dev uint32, context *fuse.Context) (newNode *nodefs.Inode, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Mkdir(name string, mode uint32, context *fuse.Context) (newNode *nodefs.Inode, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Unlink(name string, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Rmdir(name string, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Symlink(name string, content string, context *fuse.Context) (newNode *nodefs.Inode, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Rename(oldName string, newParent nodefs.Node, newName string, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Link(name string, existing nodefs.Node, context *fuse.Context) (newNode *nodefs.Inode, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, newNode *nodefs.Inode, code fuse.Status) {
+	return nil, nil, fuse.ENOSYS
+}
+func (tapper_root) Open(flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Flush(file nodefs.File, openFlags uint32, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+
+func (tapper_root) GetXAttr(attribute string, context *fuse.Context) (data []byte, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) RemoveXAttr(attr string, context *fuse.Context) fuse.Status {
+	return fuse.ENOSYS
+}
+func (tapper_root) SetXAttr(attr string, data []byte, flags int, context *fuse.Context) fuse.Status {
+	return fuse.ENOSYS
+}
+func (tapper_root) ListXAttr(context *fuse.Context) (attrs []string, code fuse.Status) {
+	return nil, fuse.ENOSYS
+}
+
+func (tapper_root) GetAttr(out *fuse.Attr, file nodefs.File, context *fuse.Context) (code fuse.Status) {
+
+	out.Mode = fuse.S_IFDIR | 0755
+
+	return fuse.OK
+}
+func (tapper_root) Chmod(file nodefs.File, perms uint32, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Chown(file nodefs.File, uid uint32, gid uint32, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Truncate(file nodefs.File, size uint64, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Utimens(file nodefs.File, atime *time.Time, mtime *time.Time, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Fallocate(file nodefs.File, off uint64, size uint64, mode uint32, context *fuse.Context) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+func (tapper_root) Read(file nodefs.File, dest []byte, off int64, context *fuse.Context) (fuse.ReadResult, fuse.Status) {
+	if file != nil {
+		return file.Read(dest, off)
+	}
+	return nil, fuse.ENOSYS
+}
+func (tapper_root) Write(file nodefs.File, data []byte, off int64, context *fuse.Context) (written uint32, code fuse.Status) {
+	if file != nil {
+		return file.Write(data, off)
+	}
+	return 0, fuse.ENOSYS
 }
